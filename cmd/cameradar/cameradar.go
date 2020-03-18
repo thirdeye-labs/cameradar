1c1711f598c99b44c4a539dbcd0a977ee5d5f1ab
oleksandr <oleksandr@thirdeye.intra>
2020-03-18 20:51:29 +0000

    username/pass provided using args, auto network range

--- cmd/cameradar/cameradar.go
+++ cmd/cameradar/cameradar.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"strings"
 	"time"
+	"net"
 
 	"github.com/Ullaakut/cameradar"
 	"github.com/Ullaakut/disgo"
@@ -14,14 +14,35 @@ import (
 	"github.com/spf13/viper"
 )
 
+
+// IP4V addresses don't usually have semicolons, not the best solution but works
+func IsIpv4Net(address string) bool {
+   return strings.Count(address, ":") < 2
+}
+
+func getLocalNetworks() []string {
+	var networks []string
+	netInterfaces, err := net.InterfaceAddrs()
+	// remove all IPv6 and localhost
+	if err == nil {
+		for _, netInterface := range netInterfaces {	
+				addr := strings.Split(netInterface.String(), "/")[0]
+				if IsIpv4Net(addr) && strings.Contains(addr, "192.168.") {
+					networks = append(networks, netInterface.String())
+				}
+		}
+	}
+   return networks
+}
+
 func parseArguments() error {
+
 	viper.SetEnvPrefix("cameradar")
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 
-	pflag.StringSliceP("targets", "t", []string{}, "The targets on which to scan for open RTSP streams - required (ex: 172.16.100.0/24)")
+	pflag.StringSliceP("targets", "t", []string{},"The targets on which to scan for open RTSP streams - required (ex: 172.16.100.0/24)")
 	pflag.StringSliceP("ports", "p", []string{"554", "5554", "8554"}, "The ports on which to search for RTSP streams")
 	pflag.StringP("custom-routes", "r", "${GOPATH}/src/github.com/Ullaakut/cameradar/dictionaries/routes", "The path on which to load a custom routes dictionary")
-	pflag.StringP("custom-credentials", "c", "${GOPATH}/src/github.com/Ullaakut/cameradar/dictionaries/credentials.json", "The path on which to load a custom credentials JSON dictionary")
 	pflag.IntP("scan-speed", "s", 4, "The nmap speed preset to use for scanning (lower is stealthier)")
 	pflag.DurationP("attack-interval", "I", 0, "The interval between each attack  (i.e: 2000ms, higher is stealthier)")
 	pflag.DurationP("timeout", "T", 2000*time.Millisecond, "The timeout to use for attack attempts (i.e: 2000ms)")
@@ -29,6 +50,8 @@ func parseArguments() error {
 	pflag.BoolP("verbose", "v", false, "Enable the verbose logs")
 	pflag.BoolP("help", "h", false, "displays this help message")
 
+	pflag.StringP("username", "u", "admin", "Username for the camera")
+	pflag.StringP("password", "P", "", "Password for the camera")
 	viper.AutomaticEnv()
 
 	pflag.Parse()
@@ -48,9 +71,18 @@ func parseArguments() error {
 		os.Exit(0)
 	}
 
-	if len(viper.GetStringSlice("targets")) == 0 {
-		pflag.Usage()
-		return errors.New("targets (-t, --targets) argument required\n    examples:\n      - 172.16.100.0/24\n      - localhost\n      - 8.8.8.8")
+	targets := viper.GetStringSlice("targets")
+	if len(targets) == 0 {
+		fmt.Println("\nNo targets provided. Detecting networks.. automatically.")
+		auto_networks := getLocalNetworks()
+		fmt.Println("\nThe following range(s) will be scanned: ")
+		fmt.Println(auto_networks)
+		targets = auto_networks
+	}
+    viper.Set("targets", targets)
+
+	if viper.GetString("password") == "" {
+		fmt.Println("\nTNo password was provided. Empty password will be used.")
 	}
 
 	return nil
@@ -67,11 +99,12 @@ func main() {
 		cameradar.WithPorts(viper.GetStringSlice("ports")),
 		cameradar.WithDebug(viper.GetBool("debug")),
 		cameradar.WithVerbose(viper.GetBool("verbose")),
-		cameradar.WithCustomCredentials(viper.GetString("custom-credentials")),
 		cameradar.WithCustomRoutes(viper.GetString("custom-routes")),
 		cameradar.WithScanSpeed(viper.GetInt("scan-speed")),
 		cameradar.WithAttackInterval(viper.GetDuration("attack-interval")),
 		cameradar.WithTimeout(viper.GetDuration("timeout")),
+		cameradar.WithUsername(viper.GetString("username")),
+		cameradar.WithPassword(viper.GetString("password")),
 	)
 	if err != nil {
 		printErr(err)
